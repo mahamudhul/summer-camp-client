9d7e1a2f4043f09461f5951c1d42b4c6c85f56eb
mahamudhul <111944934+mahamudhul@users.noreply.github.com>
2023-06-08 23:17:36 +0600

    show successfully my selected classes data from Database

--- src/Pages/DashBoard/User/MySelectedClass.jsx
+++ src/Pages/DashBoard/User/MySelectedClass.jsx
@@ -1,10 +1,85 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { useQuery } from 'react-query';
+import useAxiosSecure from '../../../Hooks/useAxiosSecure';
+import { MdDelete } from 'react-icons/md';
+
 
 const MySelectedClass = () => {
+    const [axiosSecure] = useAxiosSecure();
+
+
+    const { data: select = [], isLoading: loading, refetch } = useQuery({
+        queryKey: ['select'],
+
+        queryFn: async () => {
+            const res = await axiosSecure('/carts');
+            return res.data;
+        }
+    })
+
+    // console.log(select)
+
+
     return (
         <div>
-            <h1>Here is all selected class</h1>
+            <div>
+
+            </div>
+
+            <div>
+                <div className="overflow-x-auto">
+                    <table className="table">
+                        {/* head */}
+                        <thead>
+                            <tr>
+                                <th>
+                                    <label>
+                                        <input type="checkbox" className="checkbox" />
+                                    </label>
+                                </th>
+                                <th>Image</th>
+                                <th>Name</th>
+                                <th>Action</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+
+                        {
+                            select.map((s, index) => <tbody key={index}>
+                                {/* row 1 */}
+                                <tr>
+                                    <th>
+                                        <label>
+                                            <input type="checkbox" className="checkbox" />
+                                        </label>
+                                    </th>
+                                    <td>
+                                        <div className="mask mask-squircle w-12 h-12">
+                                            <img src={s.image} />
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <h1>{s.name}</h1>
+                                    </td>
+                                    <td>
+                                        <div>
+                                            <button ><MdDelete className='text-2xl'></MdDelete></button>
+                                        </div>
+                                    </td>
+                                    <th>
+                                        <button className="btn btn-accent btn-outline">Pay</button>
+                                    </th>
+                                </tr>
+                            </tbody>)
+                        }
+
+
+
+                    </table>
+                </div>
+            </div>
+
         </div>
     );
 };
